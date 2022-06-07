cd46459d51793cb628d54e8166f027e86bdd1451
majid salehi <majidsalehi67@gmail.com>
2022-06-07 17:49:20 +0430

    add request

--- app/Http/Requests/CompanyRequest.php
+++ app/Http/Requests/CompanyRequest.php
@@ -24,30 +24,58 @@ class CompanyRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
-            'company_email' => 'required|email',
-            'password' => 'required|confirmed',
-            'trend' => 'required',
-            'office_address' => 'required',
-            'Laboratory_address' => 'required',
-            'office_phone_number' => 'required|numeric|min:8|max:11',
-            'IDPay' => 'min:26|max:26',
-            'Accept_rules'=>'accepted'
+            'company_name' => ['required'],
+            'company_email' => ['email'],
+            'password' => ['required'],
+            'password_confirmation' => ['required','same:password'],
+            'trend' => ['required'],
+            'office_address' => ['required'],
+            'Laboratory_address' => ['required'],
+            'office_phone_number' => ['required','numeric','min:8'],
+            'IDPay' => ['min:24','max:24']
 
         ];
     }
     public function attributes()
     {
         return [
-   //
+            'company_name'=> 'نام کامل شرکت',
+            'license_number'=> 'شماره پروانه شرکت',
+            'membership_number'=> 'شماره عضویت',
+            'signature_number'=> 'شماره امضای شهرسازی',
+            'Responsible_side'=> 'سمت مسئول هیئت مدیره',
+            'trend'=> 'گرایش',
+            'CEO_name'=> 'نام مدیرعامل',
+            'CEO_lastname'=> 'نام خانوادگی مدیر عامل',
+            'CEO_id'=> 'کد ملی مدیرعامل',
+            'CEO_phone_number'=> 'شماره تماس',
+            'boss_name'=> 'نام رئیس هیئت مدیره',
+            'boss_lastname'=> 'نام خانوادگی رئیس هیئت مدیره',
+            'boss_id'=> 'کد ملی رئیس هیئت مدیره',
+            'boss_phone_number'=> 'شماره تماس رئیس هیئت مدیره',
+            'member_name'=> 'نام عضو هیئت مدیره',
+            'member_lastname'=> 'نام خانوادگی عضو هیئت مدیره',
+            'member_id'=> 'کد ملی عضو هیئت مدیره',
+            'member_phone_number'=> 'شماره تماس عضو هیئت مدیره',
+            'Laboratory_address'=> 'آدرس آزمایشگاه',
+            'Laboratory_phone_number'=> 'شماره تماس آزمایشگاه',
+            'office_address'=> 'آدرس دفتر مرکزی',
+            'office_phone_number'=> 'شماره تماس دفتر مرکزی',
+            'company_email'=> 'ایمیل',
+            'IDPay'=> 'شماره شبا',
+            'Insurance_number'=> 'شماره کارگاه بیمه',
+            'password'=> 'رمز عبور',
+            'password_confirmation'=> 'تایید رمز عبور',
+            'Accept_rules'=> 'شرایط و ضوابط',
         ];
     }
 
     public function messages()
     {
         return [
-            'required' => ' فیلد :attribute اجباری است',
-            'min' => 'فیلد :attribute باید حداقل :min تا کاراکتر داشته باشد'
+            'required' => '  :attribute اجباری است',
+            'min' => ' :attribute باید حداقل :min تا کاراکتر داشته باشد',
+            'email'=>':attribute  را با فرمت صحیح وارد کنید'
         ];
 
     }
