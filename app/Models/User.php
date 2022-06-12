8c86290075bef8439672ab9f3173637d01c8bcc4
fateme mozafari <fatemem1990@yahoo.com>
2022-06-12 10:56:12 +0430

    edit user table and model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,48 +2,48 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
+    use HasFactory;
+    protected $fillable=[
+        'company_name',
+        'license_number',
+        'membership_number',
+        'signature_number',
+        'Responsible_side',
+        'trend',
+        'CEO_name',
+        'CEO_lastname',
+        'CEO_id',
+        'CEO_phone_number',
+        'boss_name',
+        'boss_lastname',
+        'boss_id',
+        'boss_phone_number',
+        'member_name',
+        'member_lastname',
+        'member_id',
+        'member_phone_number',
+        'Laboratory_address',
+        'Laboratory_phone_number',
+        'office_address',
+        'office_phone_number',
+        'company_email',
+        'IDPay',
+        'Insurance_number',
         'password',
+        'password_confirmation',
+        'Accept_rules',
         'phone'
-    ];
+];
+    public function setTrendAttribute($value)
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-    public function tokens()
     {
-        return $this->hasMany(Token::class);
+
+        $this->attributes['trend'] = json_encode($value);
+
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,43 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('company_name');
+            $table->integer('license_number')->nullable();
+            $table->integer('membership_number')->nullable();
+            $table->integer('signature_number')->nullable();
+            $table->enum('Responsible_side',['BOSS','VEEP','MEMBER'])->nullable();
+            $table->string('trend');
+//            Profile of the CEO
+            $table->string('CEO_name')->nullable();
+            $table->string('CEO_lastname')->nullable();
+            $table->integer('CEO_id')->nullable();
+            $table->integer('CEO_phone_number')->nullable();
+//            boss of the Board
+            $table->string('boss_name')->nullable();
+            $table->string('boss_lastname')->nullable();
+            $table->integer('boss_id')->nullable();
+            $table->integer('boss_phone_number')->nullable();
+//            member of the Board
+            $table->string('member_name')->nullable();
+            $table->string('member_lastname')->nullable();
+            $table->integer('member_id')->nullable();
+            $table->integer('member_phone_number')->nullable();
+
+            $table->string('Laboratory_address');
+            $table->integer('Laboratory_phone_number')->nullable();
+            $table->string('office_address');
+            $table->integer('office_phone_number');
+            $table->string('company_email');
+            $table->bigInteger('phone');
+            $table->longText('IDPay');
+            $table->integer('Insurance_number')->nullable();
             $table->string('password');
-            $table->string('phone')->nullable();
+            $table->string('password_confirmation');
+            $table->boolean('Accept_rules')->default(false);
             $table->rememberToken();
             $table->timestamps();
+
+
         });
     }
 
